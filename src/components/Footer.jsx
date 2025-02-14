e7657ffd22a4c1ccc35da4c4c9096b7c67c1489c
Brandon N. Sangma <brancore87@gmail.com>
2025-02-14 19:25:04 +0000

    customized about page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="fixed flex items-center justify-center bottom-0 left-0 w-full bg-[#252525] text-[#AAAAAA] p-10">
+    <footer className="flex items-center justify-center bottom-0 left-0 w-full bg-[#252525] text-[#AAAAAA] p-10">
       Ⓒ 2022 #VANLIFE
     </footer>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { Link } from "react-router";
 
 export default function About() {
   return (
     <main>
-      <h1>About Page</h1>
-      <Link to="/">Home</Link>
+      <div className="bg-[url(./vanlife2.png)] mt-20 w-full bg-cover bg-center h-75 bg-no-repeat" />
+      <article className="px-5 font-inter">
+        <h1 className="text-[36px] text-[#000000] mt-10 font-bold leading-[42px]">
+          Don’t squeeze in a sedan when you could relax in a van.
+        </h1>
+        <p className="paragraph">
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch costs extra 😉)
+        </p>
+        <p className="paragraph">
+          Our team is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+      </article>
+      <section className="bg-[#FFCC8D] relative flex items-center justify-center flex-col p-10 m-10 rounded-md">
+        <h3 className="font-bold text-2xl mb-10 text-[#161616]">
+          Your destination is waiting. Your van is ready.
+        </h3>
+        <button className="bg-[#161616] px-5 bottom-5 absolute  left-10 font-inter font-bold py-3 rounded-md text-white ">
+          Explore our vans
+        </button>
+      </section>
     </main>
   );
 }
