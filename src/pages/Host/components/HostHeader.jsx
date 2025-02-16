3e7fae68d0bfc5f86d980cd7774b3a7e311c9915
Brandon N. Sangma <brancore87@gmail.com>
2025-02-16 09:14:49 +0000

    added styles to host navlinks

--- src/pages/Host/components/HostHeader.jsx
+++ src/pages/Host/components/HostHeader.jsx
@@ -1,13 +1,44 @@
 import React from "react";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 export default function HostHeader() {
+  const navLinks = [
+    {
+      path: "income",
+      name: "Income",
+    },
+    {
+      path: "host-vans",
+      name: "Vans",
+    },
+    {
+      path: "reviews",
+      name: "Reviews",
+    },
+  ];
+
   return (
     <nav className="font-inter flex items-center gap-10 font-semibold text-[#4D4D4D]">
-      <Link to="">Dashboard</Link>
-      <Link to="income">Income</Link>
-      <Link to="host-vans">Vans</Link>
-      <Link to="reviews">Reviews</Link>
+      <NavLink
+        to="/host"
+        end
+        className={({ isActive }) =>
+          `transition-all hover:text-black ${isActive ? "font-black underline" : ""}`
+        }
+      >
+        Dashboard
+      </NavLink>
+      {navLinks.map((link) => (
+        <NavLink
+          key={link.path}
+          to={link.path}
+          className={({ isActive }) =>
+            `transition-all hover:text-black ${isActive ? "font-black underline" : ""}`
+          }
+        >
+          {link.name}
+        </NavLink>
+      ))}
     </nav>
   );
 }
